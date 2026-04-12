69d068011ff6da346646bb66ed0a7b1688988fce
Felipe <felypebueno2004@gmail.com>
2026-04-12 00:53:59 -0300

    refactor(auth): tipar método register com CreateUserDTO removendo uso de any

--- frontend/src/presentation/services/AuthService.ts
+++ frontend/src/presentation/services/AuthService.ts
@@ -1,11 +1,12 @@
 import { UserRepository } from "src/infra/repositories/UserRepository";
 import { CreateUserUseCase } from "src/application/use-cases/CreateUserUseCase";
 import { LoginUserUseCase } from "src/application/use-cases/LoginUserUseCase";
+import { CreateUserDTO } from "src/application/dtos/CreateUserDTO";
 
 export class AuthService {
   private repository = new UserRepository();
 
-  async register(data: any) {
+  async register(data: CreateUserDTO) {
     const useCase = new CreateUserUseCase(this.repository);
     return await useCase.execute(data);
   }
